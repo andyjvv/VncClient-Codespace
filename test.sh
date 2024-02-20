90b022c250402d0d343488a1f06f9b9e3a0e1b91
andyjvv <65926187+andyjvv@users.noreply.github.com>
2024-02-20 16:10:37 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 mkdir -p -/setup-display && cd -/setup-display
-cat > docker-compose.yml <<EOF
+cat > docker-compose.yml <<EOF >
 services:
   display:
      image: ghcr.io/dtinth/stigervnc-docker:main
@@ -8,14 +8,14 @@ services:
      enviroment:
        VNC_GEOMETRY: 1440x900
      ports:
-       -  127.0.0.1:5900:5900
-       -  127.0.0.1:6000:6000
+       - 127.0.0.1:5900:5900
+       - 127.0.0.1:6000:6000
 
      novnc:
       image: geek1011/easy-novnc
       restart: always
       command: -a :5800 -h display --no-url-password
       ports:
-	- 127.0.0.1:%800-5800
-  E0F
+	- 127.0.0.1:5800-5800
+  EOF
   docker compose up -d
