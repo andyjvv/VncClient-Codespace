60819b41c1b8b664d6cd2377c7106f69fd41bdbd
andyjvv <65926187+andyjvv@users.noreply.github.com>
2024-02-21 14:39:15 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,16 @@
-mkdir -p-/setup-display && cd /setup-display 
-cat > docker-compose.yml <<EOF
+mkdir -p -setup-display && cd -/setup-display cat > docker-compose.yml <<EOF
 services:
 display:
-image: ghcr.io/dtinth/xtigervnc-docker:main
-tmpfs: /tmp
-restart: always
-environment:
-VNC_GEOMETRY: 1440x900
+image: gher.io/dtinth/xtigervnc-docker: main tmpfs: /tmp restart: always environment:
+VNC GEOMETRY: 1440x900
 ports:
- - 127.0.0.1:5900:5900
- - 127.0.0.1:6000:6000
+127.0.0.1:5900:5900
+127.0.0.1:6000:6000
 novnc:
-image: geek1011/easy-novnc
+image: geek1011/easy-novnic
 restart: always
-command: -a 5800 -h display --no-url-password
+command: -a 5800 h display --no-url-password
 ports:
-- 127.0.0.1:5800:5800
+127.0.0.1:5800:5800
 EOF
-docker compose up -d
+docker compose up d
